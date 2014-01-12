dc1113b7ef0874deb6f24dee5ddbc6ed0ead3bc4
Yuya Kakui <mail@kakuiyuya.com>
2014-01-12 16:26:40 +0900

    ui change

--- app/controller/items_view_controller.rb
+++ app/controller/items_view_controller.rb
@@ -1,50 +1,56 @@
-class ItemsViewController < UITableViewController
+class ItemsViewController < UIViewController
         def viewDidLoad
                 super
 
                 @feed = nil
 
-                self.navigationItem.title = "Test Reader"
-                self.view.backgroundColor = UIColor.whiteColor
+                self.navigationItem.title = "きくぽん"
+                self.view.backgroundColor = UIColor.greenColor
+
+                @index = 0
 
                 BW::HTTP.get('http://recipe4u.herokuapp.com/search.json?service=rakuten&keyword=tomato') do |response|
                         if response.ok?
                                 @feed = BW::JSON.parse(response.body.to_str)
-                                view.reloadData
+                                @label = UILabel.alloc.initWithFrame(CGRectZero)
+                                @label.text = @feed[:recipes][@index][:recipe][:title]
+                                @label.sizeToFit
+                                @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+                                self.view.addSubview @label
+                                
+                                image_data = NSData.dataWithContentsOfURL(NSURL.URLWithString(@feed[:recipes][@index][:recipe][:image]))
+                                @image = UIImageView.alloc.initWithImage(UIImage.imageWithData(image_data))
+                                @image.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+                                self.view.addSubview @image
                         else
                                 App.alert(response.error_message)
                         end
                 end
-        end
-        
-        def tableView(tableView, numberOfRowsInSection:section)
-                if @feed.nil?
-                        return 0
-                else
-                        @feed[:recipes].size
-                end
-        end
+                
+                @left_swipe = view.when_swiped do
+                        @index = @index + 1
+                        if @index < 3
+                                @label.removeFromSuperview
+                                @image.removeFromSuperview
 
-        def tableView(tableView, cellForRowAtIndexPath:indexPath)
-#                cell = tableView.dequeueReusableCellWithIdentifier('cell') ||
-#                        UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:'cell')
-
-#                cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator
-                @reuseIdentifier ||= "CELL_IDENTIFIER"
-                cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || 
-                        begin
-                                UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+                                @label = UILabel.alloc.initWithFrame(CGRectZero)
+                                @label.text = @feed[:recipes][@index][:recipe][:title]
+                                @label.sizeToFit
+                                @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+                                self.view.addSubview @label
+                                
+                                image_data = NSData.dataWithContentsOfURL(NSURL.URLWithString(@feed[:recipes][@index][:recipe][:image]))
+                                @image = UIImageView.alloc.initWithImage(UIImage.imageWithData(image_data))
+                                @image.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+                                self.view.addSubview @image
+                        else
+                                @label.removeFromSuperview
+                                @image.removeFromSuperview
+                                @label.text = "あなたにおすすめするレシピはもうありません"
+                                @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+                                self.view.addSubview @label
                         end
-
-                cell.textLabel.font = UIFont.boldSystemFontOfSize(14)
-                cell.textLabel.text = @feed[:recipes][indexPath.row][:recipe][:title]
-                cell
-        end
-
-        def tableView(taleView, didSelectRowAtIndexPath:indexPath)
-                WebViewController.new.tap do |c|
-                        c.recipe = @feed[:recipes][indexPath.row][:recipe]
-                        navigationController.pushViewController(c, animated:true)
                 end
+                @left_swipe.direction = UISwipeGestureRecognizerDirectionLeft
         end
 end
