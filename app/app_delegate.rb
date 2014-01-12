7dce97168a5c693294eb683bb304a7957e0d6cce
Yuya Kakui <mail@kakuiyuya.com>
2014-01-13 03:10:06 +0900

    improve function

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,8 +2,16 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds).tap do |w|
-            w.rootViewController = UINavigationController.alloc.initWithRootViewController(ItemsViewController.new)
             w.makeKeyAndVisible
+#            other_controller = UIViewController.new
+#            other_controller.view.backgroundColor = UIColor.purpleColor
+
+            nav_controller = UINavigationController.alloc.initWithRootViewController(ItemsViewController.new)
+
+            tab_controller = UITabBarController.new
+#            tab_controller.viewControllers = [nav_controller, other_controller]
+            tab_controller.viewControllers = [nav_controller]
+            w.rootViewController = tab_controller
     end
     true
   end
--- app/controller/items_view_controller.rb
+++ app/controller/items_view_controller.rb
@@ -5,8 +5,10 @@ class ItemsViewController < UIViewController
                 @feed  = nil
                 @index = 0
 
-                self.navigationItem.title = "きくぽん"
-                self.view.backgroundColor = UIColor.whiteColor
+                self.view.backgroundColor = UIColor.greenColor
+                self.title = "きくぽん"
+                right_button = UIBarButtonItem.alloc.initWithTitle("UserName", style: UIBarButtonItemStylePlain, target:self, action:'push')
+                self.navigationItem.rightBarButtonItem = right_button
 
                 BW::HTTP.get('http://recipe4u.herokuapp.com/search.json?service=rakuten&keyword=tomato') do |response|
                         if response.ok?
@@ -18,30 +20,66 @@ class ItemsViewController < UIViewController
                 end
                 
                 @left_swipe = view.when_swiped do
-                        @index = @index + 1
-                        @label.removeFromSuperview
-                        @image.removeFromSuperview
-                        if @index < 3
-                                self.display_view
-                        else
-                                alert = UIAlertView.new
-                                alert.message = "あなたにおすすめするレシピは\nもうありません"
-                                alert.show
-                        end
+                        self.push
                 end
                 @left_swipe.direction = UISwipeGestureRecognizerDirectionLeft
         end
         
+        def push
+                @index = @index + 1
+                @label.removeFromSuperview
+                @image.removeFromSuperview
+                if @index < 3
+                        self.display_view
+                else
+                        alert = UIAlertView.new
+                        alert.message = "あなたにおすすめするレシピは\nもうありません"
+                        alert.show
+                end
+        end
+
+        def initWithNibName(name, bundle: bundle)
+                super
+                self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag: 1)
+                self
+        end
+
         def display_view
                 @label = UILabel.alloc.initWithFrame(CGRectZero)
+                @label.backgroundColor = UIColor.yellowColor
                 @label.text = @feed[:recipes][@index][:recipe][:title]
                 @label.sizeToFit
-                @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+                @label.center = CGPointMake(self.view.frame.size.width / 2, 100)
                 self.view.addSubview @label
                 
                 image_data = NSData.dataWithContentsOfURL(NSURL.URLWithString(@feed[:recipes][@index][:recipe][:image]))
                 @image = UIImageView.alloc.initWithImage(UIImage.imageWithData(image_data))
-                @image.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+                @image.center = CGPointMake(self.view.frame.size.width / 2, 225)
                 self.view.addSubview @image
+
+                @reserve_shop_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+                @reserve_shop_button.backgroundColor = UIColor.orangeColor
+                @reserve_shop_button.sizeToFit
+                @reserve_shop_button.frame = CGRectMake(20, 340, self.view.frame.size.width-40, 50)
+                @reserve_shop_button.setTitle("予約する", forState: UIControlStateNormal)
+                @reserve_shop_button.tintColor = UIColor.blackColor
+                self.view.addSubview @reserve_shop_button
+
+                @go_shop_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+                @go_shop_button.backgroundColor = UIColor.orangeColor
+                @go_shop_button.sizeToFit
+                @go_shop_button.frame = CGRectMake(20, 400, self.view.frame.size.width-40, 50)
+                @go_shop_button.setTitle("店に行く", forState: UIControlStateNormal)
+                @go_shop_button.tintColor = UIColor.blackColor
+                self.view.addSubview @go_shop_button
+
+                @other_shop_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+                @other_shop_button.backgroundColor = UIColor.orangeColor
+                @other_shop_button.sizeToFit
+                @other_shop_button.frame = CGRectMake(20, 460, self.view.frame.size.width-40, 50)
+                @other_shop_button.setTitle("別の店に！", forState: UIControlStateNormal)
+                @other_shop_button.tintColor = UIColor.blackColor
+                self.view.addSubview @other_shop_button
+                @other_shop_button.addTarget(self, action:'push', forControlEvents:UIControlEventTouchUpInside)
         end
 end
